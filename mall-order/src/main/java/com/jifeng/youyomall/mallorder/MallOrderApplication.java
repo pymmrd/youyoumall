7784fdb0913963f0ebc826bef945f79bbf76252b
jifeng <zg196260@antgroup.com>
2022-03-26 20:52:29 +0800

    商城初始化

---
+++ mall-order/src/main/java/com/jifeng/youyomall/mallorder/MallOrderApplication.java
@@ -0,0 +1,13 @@
+package com.jifeng.youyomall.mallorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallOrderApplication.class, args);
+    }
+
+}
